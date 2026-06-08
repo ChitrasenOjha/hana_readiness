d09a20d84327b7d240e4c0149e988e9b07a2d96e
Adesh Kumar <adesh.kumar02@motherson.com>
2026-06-08 09:51:48 +0000

    second comit

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,13 +1,61 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "hanareadiness/helper/refreshTimeHelper"
-], function (Controller, refreshTimeHelper) {
+    "hanareadiness/helper/refreshTimeHelper",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, refreshTimeHelper, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("hanareadiness.controller.Dashboard", {
 
         onInit: function () {
             refreshTimeHelper.updateTime(this.getView());
+
+        },
+
+        onRunScan: function () {
+
+            var oModel = this.getOwnerComponent().getModel();
+
+            var aFilters = [
+
+                new Filter(
+                    "Package",
+                    FilterOperator.EQ,
+                    "ZPRAKHAR"
+                ),
+
+                new Filter(
+                    "Variant",
+                    FilterOperator.EQ,
+                    "ZDEFAULT"
+                )
+
+            ];
+            console.log(oModel);
+            console.log("Before read");
+
+            oModel.read("/ATCResultSet", {
+
+                filters: aFilters,
+
+                success: function (oData) {
+
+                    console.log("SUCCESS");
+                    console.log(oData.results);
+
+                },
+
+                error: function (oError) {
+
+                    console.log("ERROR");
+                    console.log(oError);
+
+                }
+
+            });
+            console.log("After read");
+
         },
 
         onRefresh: function () {
